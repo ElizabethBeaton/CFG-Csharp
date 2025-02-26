7bea2073084f83beeaab2b67f0596929d1b179d1
ElizabethBeaton <125323166+ElizabethBeaton@users.noreply.github.com>
2025-02-26 15:04:31 +0000

    Module 3 files

---
+++ module3/comparison_operators/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+
+more than
+age = 10
+age > 20
+false
+
+less than
+age = 10
+age < 50
+true
+
+
+greater than/equal to
+age = 10
+age >= 20
+false
+
+less than/equal to
+age = 10
+age <= 50
+true
+
+not equal to
+5 != 5
+false
+
+5 != 7
+true
+*/
+
+
+
+/*
+
+
+
+*/
+
+string firstString = "Hello";
+string secondString = "Yes";
+
+Console.WriteLine(firstString == secondString);
+
+/*
+
+
+if else
+
+
+int temperature = 30;
+
+if (temperature > 30) {
+Console.WriteLine("It's a hto day);
+}
+else if (temperature > 20) {
+Console.WriteLine("It's a warm day);
+} else {
+Console.WriteLine("It's a cold day")
+}
+*/
+
+
+/*
+
+
+
+AND &&
+OR ||
+NOT !
+
+
+
+
+
+*/
